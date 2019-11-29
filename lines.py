bf0ebb70861d456ef3d8c1b3a59568346cd0d748
ash11sh <srinivas.ashish369@gmail.com>
2019-11-29 11:53:44 +0530

    updated lines.py

--- lines.py
+++ lines.py
@@ -1,11 +1,16 @@
 import cv2
 import numpy as np
 
+#read image
 img = cv2.imread('images/bill1.jpg')
+
+#convert to gray
 gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+#find edges
 edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-
+#probabilistic Hough transform algorithm for line detection
 lines = cv2.HoughLinesP(edges,1,np.pi/180,120,minLineLength=25,maxLineGap=20)
 for line in lines:
     x1,y1,x2,y2 = line[0]
